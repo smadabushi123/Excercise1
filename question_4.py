c709667df584b187da20774ef68bf1306bb8c145
smadabushi123 <50666607+smadabushi123@users.noreply.github.com>
2019-05-17 13:05:27 +0530

    Update question_4.py

--- question_4.py
+++ question_4.py
@@ -4,3 +4,13 @@
     Then, the output should be:
     ['34', '67', '55', '33', '12', '98']
     ('34', '67', '55', '33', '12', '98')"""
+
+
+list1 = []
+len1 = int(input("enter how many numbers that need to be in list and tuple"))#accepting length of list and tuple from users 
+for dummy in range(0,len1): # as we need to enter multiple values in to list and tuple we are using for loop
+    numbers = int(input("enter numbers that need to be in list and tuple"))#accepting imput from user
+    list1.append(numbers)# making a list using those numbers
+    tuple1 = tuple(list1)#converting list in to tuple by using tuple() constructor
+print(list1)
+print(tuple1)
